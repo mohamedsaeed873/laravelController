b2de573c1d881a2a9ac3b46428c86e89469d9cd6
MohamedSaeed <106338523+mohamedsaeed873@users.noreply.github.com>
2022-12-29 22:42:01 +0200

    Create Foreign Keys Using Migrations

--- database/migrations/2022_12_28_204539_create_posts_table.php
+++ database/migrations/2022_12_28_204539_create_posts_table.php
@@ -16,6 +16,12 @@ return new class extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->string('title', 5);
+
+            //$table->unsignedBigInteger('user_id');
+            //$table->foreign('user_id')->references('id')->on('users');
+
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+
             $table->boolean('check')->default(0);
             $table->timestamps();
         });
